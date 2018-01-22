19d1095ff61d374d9ce113a59ecae861cf31e6f0
Peter Marshall <pmarshall1993@hotmail.com>
2018-01-22 22:22:19 +0000

    fixed bug - committed prod.js

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleInfo: {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET
+  },
+  gitHubInfo: {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET
+  },
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
